4cc9c7fbf8cbdd34fa2d17edd9481c4d2ff8f329
James Robertson <jamesofrobertson@gmail.com>
2022-07-09 16:44:20 -0400

    Add dice roller

---
+++ commands/Rolling/d6.js
@@ -0,0 +1,57 @@
+const { MessageEmbed } = require("discord.js");
+const config = require("../../botconfig/config.json");
+const ee = require("../../botconfig/embed.json");
+module.exports = {
+  name: "d6",
+  category: "Rolling",
+  aliases: ["rolld6", "rd6", "D6"],
+  cooldown: 4,
+  usage: "d6 <TEXT>",
+  description: "Rolls 2 D6 for you to choose from",
+  run: async (client, message, args, user, text, prefix) => {
+    try {
+      let result1 = Math.floor(Math.random() * 6 + 1);
+      let title1 = "Rolling Your Die";
+      let title2 = "Your D6 Result";
+      if (args[0]) {
+        let argString = "";
+        for (let element of args.values()) {
+          argString = argString + " " + element;
+        }
+        title1 = title1 + " for " + argString;
+        title2 = title2 + " for " + argString;
+      }
+      const embed = new MessageEmbed()
+        .setColor(ee.color)
+        .setTitle(title2)
+        .setFooter(ee.footertext, client.user.displayAvatarURL())
+        .addFields({ name: "Result", value: result1, inline: true });
+      message.channel
+        .send(
+          new MessageEmbed()
+            .setColor(ee.color)
+            .setFooter(ee.footertext, client.user.displayAvatarURL())
+            .setTitle(title1)
+        )
+        .then((msg) => {
+          setTimeout(function () {
+            msg
+              .delete()
+              .catch((e) => console.log("Couldn't Delete --> Ignore".gray));
+            message.channel.send(embed);
+          }, 5000);
+        });
+    } catch (e) {
+      console.log(String(e.stack).bgRed);
+      return message.channel.send(
+        new MessageEmbed()
+          .setColor(ee.wrongcolor)
+          .setFooter(ee.footertext, ee.footericon)
+          .setTitle(`❌ ERROR | An error occurred`)
+          .setDescription(`\`\`\`${e.stack}\`\`\``)
+      );
+    }
+  },
+};
+
+/** Template by Tomato#6966 | https://github.com/Tomato6966/Discord-Js-Handler-Template */
