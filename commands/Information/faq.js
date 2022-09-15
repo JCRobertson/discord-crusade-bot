643e99e081ce42cc71bf27d48f68e0dba77904f8
James Robertson <jamesofrobertson@gmail.com>
2022-09-15 16:51:33 -0400

    Create faq.js

---
+++ commands/Information/faq.js
@@ -0,0 +1,32 @@
+const { MessageEmbed } = require("discord.js");
+const config = require("../../botconfig/config.json");
+const ee = require("../../botconfig/embed.json");
+module.exports = {
+  name: "faq",
+  category: "Information",
+  aliases: ["FAQ", "eratta"],
+  cooldown: 4,
+  usage: "faq",
+  description: "Posts the Warhammer Community link for the FAQ section",
+  run: async (client, message, args, user, text, prefix) => {
+    try {
+      const embed = new MessageEmbed()
+        .setColor(ee.color)
+        .setTitle("WarCom FAQ Link")
+        .setURL("https://www.warhammer-community.com/faqs/")
+        .setFooter(ee.footertext, client.user.displayAvatarURL());
+      message.channel.send(embed);
+    } catch (e) {
+      console.log(String(e.stack).bgRed);
+      return message.channel.send(
+        new MessageEmbed()
+          .setColor(ee.wrongcolor)
+          .setFooter(ee.footertext, ee.footericon)
+          .setTitle(`❌ ERROR | An error occurred`)
+          .setDescription(`\`\`\`${e.stack}\`\`\``)
+      );
+    }
+  },
+};
+
+/** Template by Tomato#6966 | https://github.com/Tomato6966/Discord-Js-Handler-Template */
