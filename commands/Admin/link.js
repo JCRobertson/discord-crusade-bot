d91d109df9ecaa7a705a52ed7f61d7d8f62b43a6
James Robertson <jamesofrobertson@gmail.com>
2023-01-28 16:04:19 -0500

    Update link.js

--- commands/Admin/link.js
+++ commands/Admin/link.js
@@ -10,8 +10,7 @@ module.exports = {
   description: "Returns the link for the linktree",
   run: async (client, message, args, user, text, prefix) => {
     try {
-      let linkMessages = [];
-      linkMessages.push(
+      message.channel.send(
         new MessageEmbed()
           .setColor(ee.color)
           .setTitle("Vadinax Player Pack")
@@ -20,7 +19,7 @@ module.exports = {
             "https://www.goonhammer.com/the-vadinax-campaign-player-packs/"
           )
       );
-      linkMessages.push(
+      message.channel.send(
         new MessageEmbed()
           .setColor(ee.color)
           .setTitle("Crusade Balance Dataslate")
@@ -29,7 +28,7 @@ module.exports = {
             "https://drive.google.com/file/d/1jxtkUsT1353G1zyz9OUcICZrg0r6c8tT/view"
           )
       );
-      linkMessages.push(
+      message.channel.send(
         new MessageEmbed()
           .setColor(ee.color)
           .setTitle("Boarding Actions Army Mustering Rules")
@@ -38,7 +37,7 @@ module.exports = {
             "https://www.warhammer-community.com/wp-content/uploads/2023/01/qPJwZxeX8lH4HarK.pdf"
           )
       );
-      linkMessages.push(
+      message.channel.send(
         new MessageEmbed()
           .setColor(ee.color)
           .setTitle("Huzzah Planet Perplior Store Narrative Pack")
@@ -47,7 +46,7 @@ module.exports = {
             "https://docs.google.com/document/d/1Fx5a9VnHqkrZe_95M36UE7iDLSB3HpDc01m8d-6U1_A/edit?usp=sharing"
           )
       );
-      linkMessages.push(
+      message.channel.send(
         new MessageEmbed()
           .setColor(ee.color)
           .setTitle("Guide to using Administratum")
@@ -56,32 +55,20 @@ module.exports = {
             "https://docs.google.com/document/d/1RJS0B5tGWDHA8ZRItppnIIDX6yTz974-5pRRQdZylKk/edit?usp=sharing"
           )
       );
-      linkMessages.push(
+      message.channel.send(
         new MessageEmbed()
           .setColor(ee.color)
           .setTitle("Campaign Invite Link")
           .setFooter(ee.footertext, client.user.displayAvatarURL())
           .setURL("https://administratum.goonhammer.com/join?code=TBTFYG9")
       );
-      linkMessages.push(
-        new MessageEmbed()
-          .setColor(ee.color)
-          .setTitle("Huzzah Planet Perplior Store Narrative Pack")
-          .setFooter(ee.footertext, client.user.displayAvatarURL())
-          .setURL(
-            "https://docs.google.com/document/d/1Fx5a9VnHqkrZe_95M36UE7iDLSB3HpDc01m8d-6U1_A/edit?usp=sharing"
-          )
-      );
-      linkMessages.push(
+      message.channel.send(
         new MessageEmbed()
           .setColor(ee.color)
           .setTitle("PCA Linktree")
           .setFooter(ee.footertext, client.user.displayAvatarURL())
           .setURL("https://www.linktr.ee/novapca")
       );
-      linkMessages.forEach((linkMessage) => {
-        message.channel.send(linkMessage);
-      });
     } catch (e) {
       console.log(String(e.stack).bgRed);
       return message.channel.send(
